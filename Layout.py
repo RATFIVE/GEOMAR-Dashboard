a4581df324d8d1de1c1ade206b10afe7a294c39e
Marco Banzhaf <generalpegasus@web.de>
2025-02-19 12:39:00 +0100

    Füge Session State und Graphen hinzu

--- Layout.py
+++ Layout.py
@@ -4,7 +4,7 @@ import numpy as np
 import sys
 import subprocess
 from utils.frost_server import FrostServer
-from vis import make_line_plot, make_map
+
 import plotly.express as px
 import plotly.graph_objects as go
 import streamlit as st
@@ -23,10 +23,22 @@ class StreamlitApp:
 
     def __init__(self):
         #self.fig = fig
-        self.df_obs = self.get_frost_observations()
-        self.position_data = self.get_position_data()
-        self.marinas = self.get_json_data('marinas.json')
-        self.preloaded_data = self.preload_data()
+        if 'df_obs' not in st.session_state:
+            st.session_state['df_obs'] = self.get_frost_observations()
+        self.df_obs = st.session_state['df_obs']
+
+        if 'position_data' not in st.session_state:
+            st.session_state['position_data'] = self.get_position_data()
+        self.position_data = st.session_state['position_data']
+
+        if 'marinas' not in st.session_state:
+            st.session_state['marinas'] = self.get_json_data('marinas.json')
+        self.marinas = st.session_state['marinas']
+
+        if 'preloaded_data' not in st.session_state:
+            st.session_state['preloaded_data'] = self.preload_data()
+        self.preloaded_data = st.session_state['preloaded_data']
+            
         
 
     def header(self):
@@ -46,6 +58,7 @@ class StreamlitApp:
 
         #st.markdown("""<h2 style="text-align: center;">Visualisierung von Temperaturdaten</h2>""", unsafe_allow_html=True)
         with st.expander("Visualisierung", expanded=True):
+            
             self.section3()
         st.divider()
 
@@ -55,13 +68,22 @@ class StreamlitApp:
     
 
     def section1(self):
-        # get all marinas names as list
-        marinas = []
-        for marina in self.marinas:
-            marinas.append(marina['name'])
-        self.selected_marina = st.selectbox(label='Wähle den Hafen aus:', options=marinas)
+        
 
-        m = self.make_map()
+        # Nur initialisieren, wenn die Marinas noch nicht gespeichert wurden
+        if 'selected_marinas' not in st.session_state:
+            st.session_state['selected_marinas'] = [marina['name'] for marina in self.preloaded_data]
+
+        # Selectbox mit bestehender Liste aus session_state
+        self.selected_marina = st.selectbox(
+            label='Wähle den Hafen aus:', 
+            options=st.session_state['selected_marinas']
+    )
+
+        if 'map' not in st.session_state:
+            st.session_state['map'] = self.make_map()
+
+        m = st.session_state['map']
         st_folium(m, width=1000, height=500)
 
         
@@ -70,51 +92,73 @@ class StreamlitApp:
 
 
     def section2(self):
-        pass
+        for marina in self.preloaded_data:
+            if marina['name'] == self.selected_marina:
+                marina_name = marina['name']
+                marina_coordinates = marina['coordinates']
+                
 
-        # def boxed_text(text, value):
+                marina_time = marina['measurements']['time'][-1]
+                marina_water_temperature = marina['measurements']['water_temperature'][-1]
+                marina_water_heigth = marina['measurements']['water_heigth'][-1]
+                marina_wind_speed = marina['measurements']['wind_speed'][-1]
+                marina_wind_direction = marina['measurements']['wind_direction'][-1]
+                marina_air_temperature = marina['measurements']['air_temperature'][-1]
+                marina_air_pressure = marina['measurements']['air_pressure'][-1]
+                marina_air_humidity = marina['measurements']['air_humidity'][-1]
 
-        #     with st.container():
-        #         st.markdown(
-        #             f"""
-        #             <div style="text-align: center; font-size: 18px; font-weight: bold; margin-bottom: 10px;">
-        #                 {text}:
-        #             </div>
-        #             <div style="display: flex; justify-content: center;">
-        #                 <pre style="
-        #                     background-color: #f0f0f0;
-        #                     padding: 10px;
-        #                     border-radius: 5px;
-        #                     border: 1px solid #ddd;
-        #                     text-align: left;
-        #                     width: fit-content;
-        #                     max-width: 80%;
-        #                     white-space: pre-wrap;
-        #                     word-wrap: break-word;
-        #                 ">
-        #             {value}
-        #                 </pre>
-        #             </div>
-        #             """,
-        #             unsafe_allow_html=True
-        #         )
+                break
 
-        
-        # col1, col2 = st.columns(2)
-        # with col1:
-        #     boxed_text('Aktuelle Zeit', current_time)
-        #     st.divider()
-        #     boxed_text('Wassertemperatur [°C]', current_thetao)
-        #     st.divider()
-        #     boxed_text('Salinität [/10³]', current_so)
+        def boxed_text(text, value):
+
+            with st.container():
+                st.markdown(
+                    f"""
+                    <div style="text-align: center; font-size: 18px; font-weight: bold; margin-bottom: 10px;">
+                        {text}:
+                    </div>
+                    <div style="display: flex; justify-content: center;">
+                        <pre style="
+                            background-color: #f0f0f0;
+                            padding: 10px;
+                            border-radius: 5px;
+                            border: 1px solid #ddd;
+                            text-align: left;
+                            width: fit-content;
+                            max-width: 80%;
+                            white-space: pre-wrap;
+                            word-wrap: break-word;
+                        ">
+                    {value}
+                        </pre>
+                    </div>
+                    """,
+                    unsafe_allow_html=True
+                )
+
+        # convert marina time from 2025-02-19T10:00:00+00:00 to 2025-02-19 10:00:00
+        marina_time = marina_time.split('T')[0] + ' ' + marina_time.split('T')[1].split('+')[0]
+        col1, col2 = st.columns(2)
+        with col1:
+            boxed_text('Aktuelle Zeit', marina_time)
+            st.divider()
+            boxed_text('Wassertemperatur [°C]', round(marina_water_temperature, 3))
+            st.divider()
+            boxed_text('Wasserstand [m]', round(marina_water_heigth, 3))
+            st.divider()
+            boxed_text('Luft Temperatur [°C]', round(marina_air_temperature, 3))
+            
+            
                 
             
-        # with col2:
-        #     boxed_text('Nördliche Wassergeschwindigkeit [m/s]', current_vo)
-        #     st.divider()
-        #     boxed_text('Östliche Wassergeschwindigkeit [m/s]', current_uo)
-        #     st.divider()
-        #     boxed_text('Wasseroberfläche über Geoid [m]', current_zos)
+        with col2:
+            boxed_text('Wind Richtung [°]', round(marina_wind_direction,3 ))
+            st.divider()
+            boxed_text('Windgeschwindigkeit [km/h]', round(marina_wind_speed, 3))
+            st.divider()
+            boxed_text('Luftdruck [hPa]', round(marina_air_pressure, 3))
+            st.divider()
+            boxed_text('Luftfeuchtigkeit [%]', round(marina_air_humidity,3))
                        
             
             
@@ -127,45 +171,99 @@ class StreamlitApp:
 
 
     def section3(self):
+
         fig = go.Figure()
 
-        if self.selected_marina == 'Badesteg Reventlou':
-            self.df_obs = self.get_frost_observations()
+        marina_name = self.selected_marina
+        for marina in self.preloaded_data:
+            if marina['name'] == marina_name:
+                marina_name = marina['name']
+                marina_coordinates = marina['coordinates']
+                
 
-            # get the last 7 days
-            last_7_days = self.df_obs["phenomenonTime"].iloc[-1] - pd.DateOffset(days=7)
-            df_obs_last_7_days = self.df_obs[self.df_obs["phenomenonTime"] >= last_7_days]
+                marina_time = marina['measurements']['time']
+                marina_water_temperature = marina['measurements']['water_temperature']
+                marina_water_heigth = marina['measurements']['water_heigth']
+                marina_wind_speed = marina['measurements']['wind_speed']
+                marina_wind_direction = marina['measurements']['wind_direction']
+                marina_air_temperature = marina['measurements']['air_temperature']
+                marina_air_pressure = marina['measurements']['air_pressure']
+                marina_air_humidity = marina['measurements']['air_humidity']
+                break
 
-
+        def make_line_plot(x, y, title, x_label, y_label):
+            fig = go.Figure()
             fig.add_trace(go.Scatter(
-                x=df_obs_last_7_days["phenomenonTime"],
-                y=df_obs_last_7_days["result"],
+                x=x,
+                y=y,
                 mode='lines',
-                name='Temperature'
-            ))
-        else:
-            marina_data = self.marina_data.to_dataframe().reset_index()
-            marina_data = marina_data.dropna(axis=0, how='any')
-            marina_data_grouped = marina_data.groupby('time').mean().reset_index()
-            marina_data_grouped.sort_values(by='time', ascending=False, inplace=True)
-
-            fig.add_trace(go.Scatter(
-                x=marina_data_grouped['time'],
-                y=marina_data_grouped['thetao'],
-                mode='lines',
-                name='Temperature'
+                name=title
             ))
 
+            fig.update_layout(
+                title=title,
+                xaxis_title=x_label,
+                yaxis_title=y_label,
+                template='plotly_white',
+                hovermode='x unified'
+            )
 
-        fig.update_layout(
-            title='Temperatur der letzten 7 Tage',
-            xaxis_title='Zeit',
-            yaxis_title='Temperatur [°C]',
-            template='plotly_white',
-            hovermode='x unified'
-        )
+            return fig
+        
+        fig_names = ["Wassertemperatur", "Wasserstand", "Windgeschwindigkeit", "Windrichtung", "Lufttemperatur", "Luftdruck", "Luftfeuchtigkeit"]
+        figures = []
+        for name in fig_names:
+            if name == "Wassertemperatur":
+                fig = make_line_plot(marina_time, marina_water_temperature, "Wassertemperatur", "Zeit", "Temperatur [°C]")
+                figures.append(fig)
+            if name == "Wasserstand":
+                fig = make_line_plot(marina_time, marina_water_heigth, "Wasserstand", "Zeit", "Wasserstand [m]")
+                figures.append(fig)
+            if name == "Windgeschwindigkeit":
+                fig = make_line_plot(marina_time, marina_wind_speed, "Windgeschwindigkeit", "Zeit", "Windgeschwindigkeit [km/h]")
+                figures.append(fig)
+            if name == "Windrichtung":
+                fig = make_line_plot(marina_time, marina_wind_direction, "Windrichtung", "Zeit", "Windrichtung [°]")
+                figures.append(fig)
+            if name == "Lufttemperatur":
+                fig = make_line_plot(marina_time, marina_air_temperature, "Lufttemperatur", "Zeit", "Temperatur [°C]")
+                figures.append(fig)
+            if name == "Luftdruck":
+                fig = make_line_plot(marina_time, marina_air_pressure, "Luftdruck", "Zeit", "Luftdruck [hPa]")
+                figures.append(fig)
+            if name == "Luftfeuchtigkeit":
+                fig = make_line_plot(marina_time, marina_air_humidity, "Luftfeuchtigkeit", "Zeit", "Luftfeuchtigkeit [%]")
+                figures.append(fig)
 
-        st.plotly_chart(fig)
+        # if 'figures' not in st.session_state:
+        #     st.session_state['figures'] = figures
+        # figures = st.session_state['figures']
+
+
+
+        figure_selection = st.radio("", fig_names, horizontal=True)
+        if figure_selection == "Wassertemperatur":
+
+            st.plotly_chart(figures[0])
+
+        if figure_selection == "Wasserstand":
+            st.plotly_chart(figures[1])
+
+        if figure_selection == "Windgeschwindigkeit":
+            st.plotly_chart(figures[2])
+        
+        if figure_selection == "Windrichtung":
+            st.plotly_chart(figures[3])
+        
+        if figure_selection == "Lufttemperatur":
+            st.plotly_chart(figures[4])
+
+        if figure_selection == "Luftdruck":
+            st.plotly_chart(figures[5])
+
+        if figure_selection == "Luftfeuchtigkeit":
+            st.plotly_chart(figures[6])
+
 
 
 
@@ -234,35 +332,24 @@ class StreamlitApp:
            
             # zoom to Europa
             zoom_start=5,)
-
-        # T- Box
-        coordinates = self.position_data["value"][0]["location"]["coordinates"]
-        name = self.position_data["value"][0]["name"]
         
-        longitude, latitude = coordinates[0], coordinates[1]
-
-        current_temp, current_time = self.get_current_temperature_and_time()
-
-        # Add a T-Box 
-        folium.Marker(
-            [latitude, longitude],
-            popup=f"{name} Pos: {latitude}, {longitude} \n Akteulle Temperatur: {current_temp}°C",
-            tooltip=f'{name}',
-            icon=folium.Icon(icon="info-sign", color="red")
-        ).add_to(m)
-
-        # Add coordinates of marinas
-        for marina in self.marinas:
-            latitude = marina['location']['latitude']
-            longitude = marina['location']['longitude']
+        for marina in self.preloaded_data:
             name = marina['name']
+            coordinates = marina['coordinates']
+            latitude = coordinates[1]
+            longitude = coordinates[0]
+
+            current_temp = marina['measurements']['water_temperature'][-1]
+            current_temp = round(current_temp, 2)
+
+            # Add a T-Box 
             folium.Marker(
                 [latitude, longitude],
-                popup=f"{name}",
-                tooltip=f"{name}",
+                popup=f"{name} \n Aktuelle Temperatur: {current_temp}°C",
+                tooltip=f'{name}',
                 icon=folium.Icon(icon="info-sign", color="red")
             ).add_to(m)
-            
+
 
         return m
     
@@ -310,9 +397,43 @@ class StreamlitApp:
         df_omw = df_omw[cols]
         df_omw["date"] = pd.to_datetime(df_omw["date"])
 
+        marina_radius = 0.1
+
+        minimum_longitude = longitude - marina_radius
+        maximum_longitude = longitude + marina_radius
+
+        minimum_latitude = latitude - marina_radius
+        maximum_latitude = latitude + marina_radius
+
+        marina_data = ac.get_subset(
+                dataset_id="cmems_mod_glo_phy_anfc_0.083deg_PT1H-m",
+                dataset_version="202406",
+                variables=["so", "thetao", "vo", "zos", "uo"], 
+                minimum_longitude=minimum_longitude,
+                maximum_longitude=maximum_longitude,
+                minimum_latitude=minimum_latitude,
+                maximum_latitude=maximum_latitude,
+                start_datetime=one_week_before,
+                end_datetime=today,
+                minimum_depth=0.49402499198913574,
+                maximum_depth=0.49402499198913574,
+                coordinates_selection_method="strict-inside",
+                disable_progress_bar=False,
+                output_filename=output_filename
+                )
+        ac.delete_dataset(output_filename)
+
+        marina_df = marina_data.to_dataframe().reset_index()
+        marina_df = marina_df.dropna(axis=0, how='any')
+        marina_df_grouped = marina_df.groupby('time').mean().reset_index()
+        marina_df_grouped.sort_values(by='time', ascending=False, inplace=True)
+        marina_df_grouped['time'] = pd.to_datetime(marina_df_grouped['time']).dt.tz_localize('UTC')
+
+
         #merge weather data with water temperature data
         df_merged = pd.merge(df_water_temperature, df_omw, left_on='phenomenonTime', right_on='date', how='left').drop('date', axis=1)
-        #print(df_merged.columns)
+        # merge df_merge with marina data
+        df_merged = pd.merge(df_merged, marina_df_grouped, left_on='phenomenonTime', right_on='time', how='left').drop('time', axis=1)
 
         # convert datetime to isoformat
         df_merged["phenomenonTime"] = df_merged["phenomenonTime"].apply(lambda x: x.isoformat())
@@ -325,6 +446,7 @@ class StreamlitApp:
             'measurements': {
                 'time': df_merged['phenomenonTime'].tolist(),
                 'water_temperature': df_merged['result'].tolist(),
+                'water_heigth': df_merged['zos'].tolist(),
                 'wind_speed': df_merged['wind_speed_10m'].tolist(),
                 'wind_direction': df_merged['wind_direction_10m'].tolist(),
                 'air_temperature': df_merged['temperature_2m'].tolist(),
@@ -395,6 +517,7 @@ class StreamlitApp:
                 'measurements': {
                     'time': marina_df_grouped['time'].tolist(),
                     'water_temperature': marina_df_grouped['thetao'].tolist(),
+                    'water_heigth': marina_df_grouped['zos'].tolist(),
                     'wind_speed': df_merged['wind_speed_10m'].tolist(),
                     'wind_direction': df_merged['wind_direction_10m'].tolist(),
                     'air_temperature': df_merged['temperature_2m'].tolist(),
@@ -405,9 +528,10 @@ class StreamlitApp:
 
 
             preload_data.append(marina_data)
-            break
+            #print(preload_data[0]['measurements'].keys())
+            #break
 
-        # # save reload_data to json file
+        # # # save reload_data to json file
         # with open('preload_data.json', 'w') as f:
         #     json.dump(preload_data, f, indent=4, ensure_ascii=True)
         
@@ -422,54 +546,11 @@ class StreamlitApp:
 
 
 
-        # minimum_longitude = longitude - radius
-        # maximum_longitude = longitude + radius
-
-        # minimum_latitude = latitude - radius
-        # maximum_latitude = latitude + radius
-        
-
-        # self.marina_data = ac.get_subset(
-        #         dataset_id="cmems_mod_glo_phy_anfc_0.083deg_PT1H-m",
-        #         dataset_version="202406",
-        #         variables=["so", "thetao", "vo", "zos", "uo"], 
-        #         minimum_longitude=minimum_longitude,
-        #         maximum_longitude=maximum_longitude,
-        #         minimum_latitude=minimum_latitude,
-        #         maximum_latitude=maximum_latitude,
-        #         start_datetime=one_week_before,
-        #         end_datetime=today,
-        #         minimum_depth=0.49402499198913574,
-        #         maximum_depth=0.49402499198913574,
-        #         coordinates_selection_method="strict-inside",
-        #         disable_progress_bar=False,
-        #         output_filename=output_filename
-        #         )
-        # ac.delete_dataset(output_filename)
-
-
-
-
-
-
-            
-        
-        
-
-
-
     def run_streamlit(self):
         subprocess.run([sys.executable, "-m", "streamlit", "run", "Layout.py"])
 
 if __name__ == '__main__':
 
     app = StreamlitApp()
-
-
-    content = app.preload_data()
-
-    
-
-
-    #app.header()
+    app.header()
     #app.sidebar()
